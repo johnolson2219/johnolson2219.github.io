0da46a750295ba2c850f7891df2443ec7c572e5e
kakashitheaf <kakashi.theaf0109@gmail.com>
2023-08-25 23:46:20 -0400

    build create

---
+++ static/js/8.38c90b23.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[8],{88:function(t,e,p){"use strict";p.r(e);p(0),p(90);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=8.38c90b23.chunk.js.map
